464971517bc8e4200d5313a2b53f3484ef168ec9
Andrii Beznosov <andrey301288@gmail.com>
2022-08-21 16:19:36 +0300

    finalization of forms

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,22 +3,30 @@
 //? При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
 //? Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
 import throttle from 'lodash.throttle';
-const form = document.querySelector('.feedback-form');
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
 const STORAGE_KEY = 'feedback-form-state';
 
-form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onForm, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onForm, 500));
 
 onLocalStorage();
 
-const onFormData = {};
+let onFormData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 function onFormSubmit(e) {
   e.preventDefault();
-  e.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
-  console.log(`User mail: ${onFormData.email} `);
-  console.log(`User message: ${onFormData.message}`);
+  if (e.target[0].value !== '' && e.target[1].value !== '') {
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(onFormData);
+    onFormData = {};
+  }
 }
 
 function onForm(e) {
@@ -28,9 +36,20 @@ function onForm(e) {
 
 function onLocalStorage() {
   const sevedMessage = localStorage.getItem(STORAGE_KEY);
+
   if (sevedMessage) {
     const sevedMessageParse = JSON.parse(sevedMessage);
-    form.email.value = sevedMessageParse.email || '';
-    form.message.value = sevedMessageParse.message || '';
+
+    if (sevedMessageParse['email'] !== undefined) {
+      refs.input.value = sevedMessageParse['email'];
+    } else {
+      refs.input.value = '';
+    }
+
+    if (sevedMessageParse['message'] !== undefined) {
+      refs.textarea.value = sevedMessageParse['message'];
+    } else {
+      refs.textarea.value = '';
+    }
   }
 }
